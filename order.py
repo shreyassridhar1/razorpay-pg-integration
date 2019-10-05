38d9557aa8b640d5804340cb3c9845c3d68d4670
shreyas_sridhar <you@example.com>
2019-10-05 14:49:39 +0530

    changes

--- order.py
+++ order.py
@@ -1,13 +1,23 @@
 import razorpay
 from flask import Flask, request, render_template
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__, static_folder="static", static_url_path='', template_folder='templates')
 
 
 @app.route('/order', methods=['GET'])
 def create_order():
-    key_id = "rzp_test_rP3amifhM3ptE4"
-    key_secret = "413xw4HpVplajW4UUCelm8pE"
+    params = request.args
+    key_id = params.get('key_id')
+
+    if key_id == None:
+        return BadRequest()
+
+    key_secret = params.get('key_secret')
+
+    if key_secret == None:
+        return BadRequest()
+
     razorpay_client = razorpay.Client(auth=(key_id, key_secret))
     DATA = {
         "amount": 500000,
@@ -22,8 +32,8 @@ def create_order():
 
 @app.route('/charge', methods=['POST'])
 def app_charge():
-    key_id = "rzp_test_rP3amifhM3ptE4"
-    key_secret = "413xw4HpVplajW4UUCelm8pE"
+    key_id = request.form['key_id']
+    key_secret = request.form['key_secret']
     razorpay_client = razorpay.Client(auth=(key_id, key_secret))
     payment_id = request.form['razorpay_payment_id']
     order_id = request.form['razorpay_order_id']
